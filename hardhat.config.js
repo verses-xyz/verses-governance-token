4e30c67f5bf1e0d68f1c6e884672a7185f5f55ae
Raymond Z <raykyri@gmail.com>
2022-02-08 12:26:25 -0500

    configure testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,13 +2,15 @@ require("@nomiclabs/hardhat-waffle");
 require("dotenv").config()
 
 const POLYGON_ALCHEMY_API_KEY = process.env.POLYGON_ALCHEMY_API_KEY;
-const MUMBAI_ALCHEMY_API_KEY = process.env.MUMBAI_ALCHEMY_API_KEY;
+const RINKEBY_ALCHEMY_API_KEY = process.env.RINKEBY_ALCHEMY_API_KEY;
 let DEPLOYER_PRIVATE_KEY = process.env.DEPLOYER_PRIVATE_KEY;
 
 if (!DEPLOYER_PRIVATE_KEY) {
   DEPLOYER_PRIVATE_KEY = "0x" + require('crypto').randomBytes(32).toString('hex');
   console.log("Generated new private key:", DEPLOYER_PRIVATE_KEY);
   console.log("This probably won't work unless running on localhost!");
+} else {
+  console.log("Using private key in .env...");
 }
 
 // The next line is part of the sample project, you don't need it in your
@@ -18,14 +20,14 @@ require("./tasks/faucet");
 
 module.exports = {
     solidity: "0.8.11",
-    defaultNetwork: "mumbai",
+    defaultNetwork: "localhost",
     networks: {
 	polygon: {
-	    url: `https://eth-polygon.alchemyapi.io/v2/${POLYGON_ALCHEMY_API_KEY}`,
+	    url: `https://polygon-mainnet.g.alchemy.com/v2/${POLYGON_ALCHEMY_API_KEY}`,
 	    accounts: [`${DEPLOYER_PRIVATE_KEY}`]
 	},
-	mumbai: {
-	    url: `https://polygon-mumbai.g.alchemy.com/v2/${MUMBAI_ALCHEMY_API_KEY}`,
+	rinkeby: {
+	    url: `https://eth-rinkeby.g.alchemy.com/v2/${RINKEBY_ALCHEMY_API_KEY}`,
 	    accounts: [`${DEPLOYER_PRIVATE_KEY}`]
 	}
     }
