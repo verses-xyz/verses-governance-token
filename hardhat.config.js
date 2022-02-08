8b40aa8bacb50d54796c1108f775b52f1098af2a
Raymond Z <raykyri@gmail.com>
2022-02-08 00:25:52 -0500

    move config variables to template

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,15 @@
 require("@nomiclabs/hardhat-waffle");
+require("dotenv").config()
 
-const ROPSTEN_ALCHEMY_API_KEY = "3N41dUGoRgXtsjlc8yV3fsiJxxZOrTh5";
-const MUMBAI_ALCHEMY_API_KEY = "NysK-hrvnLiLmTNBC-RrwME4Vq13yhvz";
-const PRIVATE_KEY = "766572736573ffffffffffffffffffffffffffffffffffffffffffffffffffff"; // base64 encoded "verses"
+const POLYGON_ALCHEMY_API_KEY = process.env.POLYGON_ALCHEMY_API_KEY;
+const MUMBAI_ALCHEMY_API_KEY = process.env.MUMBAI_ALCHEMY_API_KEY;
+let DEPLOYER_PRIVATE_KEY = process.env.DEPLOYER_PRIVATE_KEY;
+
+if (!DEPLOYER_PRIVATE_KEY) {
+  DEPLOYER_PRIVATE_KEY = "0x" + require('crypto').randomBytes(32).toString('hex');
+  console.log("Generated new private key:", DEPLOYER_PRIVATE_KEY);
+  console.log("This probably won't work unless running on localhost!");
+}
 
 // The next line is part of the sample project, you don't need it in your
 // project. It imports a Hardhat task definition, that can be used for
@@ -11,14 +18,15 @@ require("./tasks/faucet");
 
 module.exports = {
     solidity: "0.8.11",
+    defaultNetwork: "mumbai",
     networks: {
-	ropsten: {
-	    url: `https://eth-ropsten.alchemyapi.io/v2/${ROPSTEN_ALCHEMY_API_KEY}`,
-	    accounts: [`${PRIVATE_KEY}`]
+	polygon: {
+	    url: `https://eth-polygon.alchemyapi.io/v2/${POLYGON_ALCHEMY_API_KEY}`,
+	    accounts: [`${DEPLOYER_PRIVATE_KEY}`]
 	},
 	mumbai: {
 	    url: `https://polygon-mumbai.g.alchemy.com/v2/${MUMBAI_ALCHEMY_API_KEY}`,
-	    accounts: [`${PRIVATE_KEY}`]
+	    accounts: [`${DEPLOYER_PRIVATE_KEY}`]
 	}
     }
 };
